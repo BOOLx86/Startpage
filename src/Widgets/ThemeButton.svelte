fef587835d7ce8c6bad51435f4a448738ab087b8
BOOLx86 <ole.plechinger@online>
2022-01-04 02:13:39 +0100

    fixed theme storage and made theme button prettier

--- src/Widgets/ThemeButton.svelte
+++ src/Widgets/ThemeButton.svelte
@@ -1,13 +1,13 @@
 <script>
     const themes = [
-        { 'class': "light",     'icon': "far fa-sun"    },
-        { 'class': "dark",      'icon': "far fa-moon"   },
-        { 'class': "neon",      'icon': "far fa-image"  }
+        { "class": "light",     "icon": "far fa-sun"    },
+        { "class": "dark",      "icon": "far fa-moon"   },
+        { "class": "neon",      "icon": "fas fa-bolt"   }
     ]
     
     // load theme or set to default
-    let theme = localStorage.getItem('theme') === null? localStorage.getItem('theme') : themes[0];
-    localStorage.setItem('theme', theme);
+    let theme = localStorage.getItem('theme') !== null? JSON.parse(localStorage.getItem('theme')) : themes[0];
+    localStorage.setItem('theme', JSON.stringify(theme));
     
     // initial apply theme
     let icon = theme.icon;
@@ -17,7 +17,7 @@
     // set index to corresponding place in 'themes', or to -1 if not in there
     let index = -1;
     for (let i in themes) {
-        if (themes[i].class = theme.class) { index = i; break; }
+        if (themes[i].class == theme.class) { index = i; break; }
     }
 
     function apply_theme() {
@@ -29,14 +29,15 @@
 
     function switch_theme() {
         theme = themes[++index % themes.length]; // cycle between themes
-        localStorage.setItem('theme', theme);
+        localStorage.setItem('theme', JSON.stringify(theme));
 
         apply_theme();
     };
 </script>
 
-
-<i on:click="{switch_theme}" id="theme_button" class={icon}></i>
+<div on:click="{switch_theme}" id="theme_button">
+    <i class={icon}></i>
+</div>
 
 <style>
     #theme_button {
@@ -45,6 +46,20 @@
         right: 0;
         margin: 1em;
 
+        width: 2.5em;
+        height: 2.5em;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        border-radius: 50%;
+    }
+    #theme_button:hover {
+        background-color: var(--sbg);
+    }
+
+    #theme_button > i {
         font-size: 1.5em;
     }
 </style>
